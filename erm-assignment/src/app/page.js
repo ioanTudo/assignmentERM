7de176cacc9464780d63a50a472d2bb0e3bd8128
Ioan Tudor <ioantudor@Laptop-Ioan.local>
2025-04-06 21:17:26 +0300

    polishing the app

--- erm-assignment/src/app/page.js
+++ erm-assignment/src/app/page.js
@@ -10,6 +10,12 @@ export default function Home() {
   const { fetchedData, loading, error } = useData();
   const [visibility, setVisibility] = useState("none");
 
+  const handleOptions = (e) => {
+    setSelectedSkipId(Number(e.target.value));
+
+    setVisibility("none");
+  };
+
   const formatCurrency = (amount) =>
     new Intl.NumberFormat("en-GB", {
       style: "currency",
@@ -20,12 +26,14 @@ export default function Home() {
 
   useEffect(() => {
     console.log("Selected ID:", selectedSkipId);
+
     console.log("Selected skip:", selectedSkip);
   }, [selectedSkipId]);
 
   return (
     <div className={style.content_wrapper}>
-      {loading && <p>Loading data...</p>}
+      <h1 className={style.pageHeader}>select skip size</h1>
+      {loading && <span className={style.loader}></span>}
       {error && <p style={{ color: "red" }}>{error}</p>}
 
       {!loading && !error && (
@@ -39,49 +47,69 @@ export default function Home() {
 
           <select
             className={style.select}
-            onChange={(e) => setSelectedSkipId(Number(e.target.value))}
+            onChange={handleOptions}
             value={selectedSkipId || ""}
           >
             <option disabled value="">
-              Select skip size
+              Select skip size in yards
             </option>
             {fetchedData.map((item) => (
               <option key={item.id} value={item.id}>
-                {item.size} yards
+                {item.size}
               </option>
             ))}
           </select>
 
           {selectedSkip && (
             <div>
-              <p>Size: {selectedSkip.size} yards</p>
-              <p>Hire period: {selectedSkip.hire_period_days} days</p>
-              <p>
-                Price before VAT:{" "}
-                {formatCurrency(selectedSkip.price_before_vat)}
+              <p className={style.yardsAmount}>
+                {selectedSkip.size} yards skip
               </p>
+              <p>
+                <strong>Hiring period</strong>: {selectedSkip.hire_period_days}{" "}
+                days
+              </p>
+              <p>
+                <strong>Price:</strong>{" "}
+                {selectedSkip.price_before_vat === 0.0 ? (
+                  <span style={{ color: "red", fontWeight: 500 }}>
+                    No price yet,{" "}
+                    <a href="/contact" style={{ textDecoration: "underline" }}>
+                      contact us
+                    </a>
+                  </span>
+                ) : (
+                  `${formatCurrency(selectedSkip.price_before_vat)} per week`
+                )}
+              </p>
+              <div className={style.buttons_container}>
+                <button
+                  className={style.selectBtn}
+                  style={{ display: visibility === "block" ? "none" : "block" }}
+                  onClick={() => setVisibility("block")}
+                >
+                  Select
+                </button>
 
-              <button
-                className={style.selectBtn}
-                style={{ display: visibility === "block" ? "none" : "block" }}
-                onClick={() => setVisibility("block")}
-              >
-                Select
-              </button>
-
-              <button
-                className={style.confirm_button}
-                style={{ display: visibility }}
-              >
-                Continue
-              </button>
-              <button
-                style={{ display: visibility }}
-                onClick={() => setVisibility("none")}
-                className={style.selectBtn}
-              >
-                Back
-              </button>
+                <button
+                  className={style.confirm_button}
+                  style={{ display: visibility }}
+                  onClick={() =>
+                    alert(
+                      `Continue to the next page for ${selectedSkip.size} yards skip`
+                    )
+                  }
+                >
+                  Continue
+                </button>
+                <button
+                  style={{ display: visibility }}
+                  onClick={() => setVisibility("none")}
+                  className={style.selectBtn}
+                >
+                  Back
+                </button>
+              </div>
             </div>
           )}
         </div>
